ccc671bb53f9f73a4a80958eb4a498e37777bc9e
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-10-25 19:36:07 -0300

    Refactor rh select  handlers

--- src/Components/Select/Select.jsx
+++ src/Components/Select/Select.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import './style.css';
-import fruitDrop from '../../assets/images/fruit-drop.png';
+import happy from '../../assets/images/happy.png';
+import happy2 from '../../assets/images/happy2.png';
+import happy3 from '../../assets/images/happy3.png';
+import happy4 from '../../assets/images/happy4.png';
+import sad from '../../assets/images/sad-drop.png';
+import sad2 from '../../assets/images/sad-drop2.png';
+import sad3 from '../../assets/images/sad-drop3.png';
 
-const bloodType = [
+const allBloodTypes = [
   { type: "O" },
   { type: "A" },
   { type: "B" },
   { type: "AB" }
 ]
+const imgCompatible = [happy, happy2, happy3, happy4]
+const imgNotCompatible = [sad, sad2, sad3]
 
 export function Select() {
   const [result, setResult] = useState("")
@@ -16,16 +24,19 @@ export function Select() {
   const [rhDonor, setRhDonor] = useState('+')
   const [rhRecipient, setRhRecipient] = useState('+')
 
-  const bloodTypes = {
-    donor: {
-      rh: "",
-      bloodType: ""
-    },
-    recipient: {
-      rh: "",
-      bloodType: ""
-    }
-  }
+  const randCompatibleImg = imgCompatible[Math.floor(Math.random() * imgCompatible.length)];
+  const randNotCompatibleImg = imgNotCompatible[Math.floor(Math.random() * imgNotCompatible.length)];
+
+  // const bloodTypes = {
+  //   donor: {
+  //     rh: "+",
+  //     bloodType: "O"
+  //   },
+  //   recipient: {
+  //     rh: "+",
+  //     bloodType: "O"
+  //   }
+  // }
 
   const handleSelectDonor = (e) => {
     setDonorType(e.target.value)
@@ -33,17 +44,21 @@ export function Select() {
   const handleSelectRecipient = (e) => {
     setRecipientType(e.target.value)
   }
-  const handleRhDonor = (e) => {
-    setRhDonor(e.target.value)
-  }
-  const handleRhRecipient = (e) => {
-    console.log(e.target.name, e.target.value)
-    setRhRecipient(e.target.value)
-  }
-
-  // function isCompatible() {
-  //   (rhDonor === "+" && rhRecipient === "-")
+  // const handleRhDonor = (e) => {
+  //   setRhDonor(e.target.value)
   // }
+  // const handleRhRecipient = (e) => {
+  //   console.log(e.target.name, e.target.value)
+  //   setRhRecipient(e.target.value)
+  // }
+
+  function isCompatible(e) {
+    e.target.name === "rh-donor" ? setRhDonor(e.target.value) : setRhRecipient(e.target.value)
+
+    return (e.target.name === "rh-recipient" && e.target.value === "+")
+      && (e.target.name === "rh-donor" && e.target.value === "-")
+    // return (rhDonor === "+" && rhRecipient === "-")
+  }
 
   // function message() {
   //   boo ? setResult(`O tipo ${donorType}${rhDonor} pode doar para ${recipientType}${rhRecipient}`) :
@@ -67,15 +82,15 @@ export function Select() {
         }
       } else if (donorType === "B") {
         if (recipientType === "B" || recipientType === "AB") {
-          setResult(`O tipo ${donorType}${rhDonor} pode doar para ${recipientType}${rhRecipient}`)
+          return setResult(`O tipo ${donorType}${rhDonor} pode doar para ${recipientType}${rhRecipient}`)
         } else {
-          setResult(`O tipo ${donorType}${rhDonor} não pode doar para ${recipientType}${rhRecipient}`)
+          return setResult(`O tipo ${donorType}${rhDonor} não pode doar para ${recipientType}${rhRecipient}`)
         }
       } else if (donorType === "AB") {
         if (recipientType === "AB") {
-          setResult(`O tipo AB${rhDonor} pode doar para ${recipientType}${rhRecipient}`)
+          return setResult(`O tipo AB${rhDonor} pode doar para ${recipientType}${rhRecipient}`)
         } else {
-          setResult(`O tipo AB${rhDonor} não pode doar para ${recipientType}${rhRecipient}`)
+          return setResult(`O tipo AB${rhDonor} não pode doar para ${recipientType}${rhRecipient}`)
         }
       }
     }
@@ -89,13 +104,13 @@ export function Select() {
             Tipo sanguíneo doador
         </label>
           <select value={donorType} onClick={handleResult} onChange={handleSelectDonor} name="donor">
-            {bloodType.map((item, i) => (
+            {allBloodTypes.map((item, i) => (
               <option key={i} value={item.type}>{item.type}</option>
             ))}
           </select>
 
           <label>Rh</label>
-          <select value={rhDonor} onClick={handleResult} onChange={handleRhDonor} name="rh-donor">
+          <select value={rhDonor} onClick={handleResult} onChange={isCompatible} name="rh-donor">
             <option value="+">+</option>
             <option value="-">-</option>
           </select>
@@ -103,14 +118,14 @@ export function Select() {
 
         <div className="recipient-options">
           <label>Tipo sanguíneo receptor</label>
-          <select value={recipientType} onClick={handleResult} onChange={handleSelectRecipient} name="receptor">
-            {bloodType.map((item, i) => (
+          <select value={recipientType} onClick={handleResult} onChange={handleSelectRecipient} name="recipient">
+            {allBloodTypes.map((item, i) => (
               <option key={i} value={item.type}>{item.type}</option>
             ))}
           </select>
 
           <label>Rh</label>
-          <select value={rhRecipient} onClick={handleResult} onChange={handleRhRecipient} name="rh-receptor">
+          <select value={rhRecipient} onClick={handleResult} onChange={isCompatible} name="rh-recipient">
             <option value="+">+</option>
             <option value="-">-</option>
           </select>
@@ -119,6 +134,8 @@ export function Select() {
 
       <section className="result">
         {result}
+        <img hidden={true} src={randCompatibleImg} alt="blood-drop" />
+        <img hidden={true} src={randNotCompatibleImg} alt="blood-drop" />
       </section>
     </main>
   )

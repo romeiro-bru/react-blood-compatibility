de4225f67ede94492f2df63eef48386e0a2400dd
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-10-27 12:44:41 -0300

    Add github icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import { Select } from './Components/Select/Select';
+import github from './assets/images/github.png';
+
+const Icon = () => {
+  return (
+    <a href="https://github.com/romeiro-bru/typescript-ghibli-movies" target="_blank" rel="noopener noreferrer">
+      <img className="github" src={github} alt="icon" />
+    </a>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Blood Compatibility</h1>
       <Select />
+      <Icon />
     </div>
   );
 }

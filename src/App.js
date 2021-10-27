62e2cac45e724b55db7f76557fd6e3dece923bd0
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-10-27 16:20:46 -0300

    Add footer,Add media query

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { Select } from './Components/Select/Select';
 import github from './assets/images/github.png';
 import bloodDonation from './assets/images/blood-donation.png';
 
-const Button = () => {
+const Footer = () => {
   return (
-    <p>
-      Quer ser um doador?
-      <a href="https://www.gov.br/saude/pt-br/composicao/saes/sangue" target="_blank" rel="noopener noreferrer">
-        <button>Saiba mais</button>
-      </a>
-      <img src={bloodDonation} alt="blood-donation" />
-    </p>
-  )
-}
-const Icon = () => {
-  return (
-    <a href="https://github.com/romeiro-bru/typescript-ghibli-movies" target="_blank" rel="noopener noreferrer">
-      <img className="github" src={github} alt="icon" />
-    </a>
+    <footer>
+      <p>
+        Quer ser um doador?
+        <a href="https://www.gov.br/saude/pt-br/composicao/saes/sangue" target="_blank" rel="noopener noreferrer">
+          <button>Saiba mais</button>
+        </a>
+        <img src={bloodDonation} alt="blood-donation" />
+      </p>
+      <div>
+        <a href="https://github.com/romeiro-bru/typescript-ghibli-movies" target="_blank" rel="noopener noreferrer">
+          <img className="github" src={github} alt="icon" />
+        </a>
+      </div>
+    </footer>
   )
 }
 
@@ -27,8 +27,7 @@ function App() {
     <div className="App">
       <h1>Blood Compatibility</h1>
       <Select />
-      <Button />
-      <Icon />
+      <Footer />
     </div>
   );
 }

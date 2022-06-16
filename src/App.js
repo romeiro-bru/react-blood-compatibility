f74903230c865b252012bd71a74f48eb7bdb389e
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-06-16 19:55:12 -0300

    Hide img using aria-hidden

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ const Footer = () => {
     <footer className="flex justify-center">
       <div>
         <span>Quer ser um doador? </span>
-        <button>
-          <a href="https://www.gov.br/saude/pt-br/composicao/saes/sangue" target="_blank" rel="noopener noreferrer">
+        <a aria-labelledby="doação de sangue" href="https://www.gov.br/saude/pt-br/composicao/saes/sangue" target="_blank" rel="noopener noreferrer">
+          <button id="doação de sangue">
             Saiba mais
-        </a>
         </button>
+        </a>
       </div>
       <div>
         <a tabIndex="0" href="https://github.com/romeiro-bru/react-blood-compatibility" target="_blank" rel="noopener noreferrer">
-          <img className="github" src={github} alt="github" />
+          <img className="github" src={github} aria-hidden="true" alt="github opens in new window" />
         </a>
       </div>
     </footer>
